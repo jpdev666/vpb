4ad40cb3d4bcceebc886dfa4dd67360ee332b80f
ved2pj <lancee.website@gmail.com>
2024-02-08 11:54:14 +0800

    Update base path

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/vpb/",
+  base: "/",
 
   lang: "zh-CN",
   title: "文档演示",
